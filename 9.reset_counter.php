eaed1367661b20089bde687df4bcc696f2f1f917
Lowei1225 <jack20041225@gmail.com>
2025-04-11 21:03:16 +0800

    Update 9.reset_counter.php

--- 9.reset_counter.php
+++ 9.reset_counter.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    unset($_SESSION["counter"]);
+    unset($_SESSION["counter"]); #使用者在 8.counter.php 頁面上點重置 開啟此檔 刪除$_SESSION["counter"]
     echo "counter重置成功....";
-    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
+    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>"; #2秒後自動跳回去 計數器重新開始
 
 ?>
