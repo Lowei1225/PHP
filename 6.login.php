7803ea73ba8a6f53402e586efd722b55582e6c27
Lowei1225 <jack20041225@gmail.com>
2025-04-11 20:46:32 +0800

    Update 6.login.php

--- 6.login.php
+++ 6.login.php
@@ -1,13 +1,15 @@
 <?php
-
+#mysqli_connect() 建立資料庫連結
 $conn=mysqli_connect("db4free.net","immust","immustimmust","immust");
+   #mysqli_query() 從資料庫查詢資料
 $result=mysqli_query($conn, "select * from user");
+   #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
 $login=false;
 while($row=mysqli_fetch_array($result)){
     if(($_POST["id"]==$row["id"])&&($_POST["pwd"]==$row["pwd"]))
         $login=true;
 }
-
+#若登入與資料庫帳密不符將慧登入失敗且重新登入，符合則會進入
 if ($login==true)
     echo"登入成功";
 else
