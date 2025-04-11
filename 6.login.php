3fc9b09ecce2b454b5f19305a7d5eee9800c783e
Lowei1225 <jack20041225@gmail.com>
2025-04-11 19:53:33 +0800

    Update 6.login.php

--- 6.login.php
+++ 6.login.php
@@ -1,1 +1,16 @@
+<?php
 
+$conn=mysqli_connect("db4free.net","immust","immustimmust","immust");
+$result=mysqli_query($conn, "select * from user");
+$login=false;
+while($row=mysqli_fetch_array($result)){
+    if(($_POST["id"]==$row["id"])&&($_POST["pwd"]==$row["pwd"]))
+        $login=true;
+}
+
+if ($login==true)
+    echo"登入成功";
+else
+    echo"登入失敗";
+
+?>
