1873b78c44a27d6eb5af0c47aaf993764e1b403d
Lowei1225 <jack20041225@gmail.com>
2025-04-11 19:54:06 +0800

    Update 7.bulletin.php

--- 7.bulletin.php
+++ 7.bulletin.php
@@ -1,1 +1,17 @@
+<?php
 
+$conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+$result=mysqli_query($conn, "select * from bulletin");
+echo "<table border=1><tr><td>布告欄編號</td><td>布告型態</td><td>標題</td><td>布告內容</td>"
+while($row=mysqli_fetch_array($result)){
+    echo $row["bid"];
+    echo" ";
+    echo $row["type"];
+    echo" ";
+    echo $row["title"];
+    echo" ";
+    echo $row["content"];
+    echo" ";
+    echo $row["time"];
+    echo"<br>";
+}
