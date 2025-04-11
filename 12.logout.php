f1ab6b638f4b1d65321d8f770e1ac2a4014d72b3
Lowei1225 <jack20041225@gmail.com>
2025-04-11 21:13:21 +0800

    Update 12.logout.php

--- 12.logout.php
+++ 12.logout.php
@@ -1,7 +1,7 @@
 <?php
-    session_start();
-    unset($_SESSION["id"]);
+    session_start(); #開啟
+    unset($_SESSION["id"]); #清除登入資訊
     echo "登出成功....";
-    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
+    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";  #3秒後自動跳轉 到 2.login.html 登入頁面
 
 ?>
