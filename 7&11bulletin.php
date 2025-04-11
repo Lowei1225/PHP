5c19494d43cfa6c393e02e3e78c8eff4b54686bc
Lowei1225 <jack20041225@gmail.com>
2025-04-11 20:51:56 +0800

    Update 7&11bulletin.php

--- 7&11bulletin.php
+++ 7&11bulletin.php
@@ -1,15 +1,15 @@
 <?php
     error_reporting(0);
-    session_start();
+    session_start(); #啟用 PHP session，讓我們可以存取 $_SESSION["id"]
     if (!$_SESSION["id"]) {
-        echo "請先登入";
-        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+        echo "請先登入"; #如果沒有登入（沒有 $_SESSION["id"]），就顯示「請先登入」,3 秒後自動跳轉回登入頁 2.login.html
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>"; 
     }
-    else{
+    else{#登入成功後顯示
         echo "歡迎, ".$_SESSION["id"]."[<a href=12.logout.php>登出</a>] [<a href=18.user.php>管理使用者</a>] [<a href=22.bulletin_add_form.php>新增佈告</a>]<br>";
         $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
         $result=mysqli_query($conn, "select * from bulletin");
-        echo "<table border=2><tr><td></td><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
+        echo "<table border=2><tr><td></td><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";#標題列
         while ($row=mysqli_fetch_array($result)){
             echo "<tr><td><a href=26.bulletin_edit_form.php?bid={$row["bid"]}>修改</a> 
             <a href=28.bulletin_delete.php?bid={$row["bid"]}>刪除</a></td><td>";
