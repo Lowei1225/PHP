4d56a0e0645f7bde9e72cb7839d3fbad904faf0c
Lowei1225 <jack20041225@gmail.com>
2025-04-11 19:53:00 +0800

    Update 5.while.php

--- 5.while.php
+++ 5.while.php
@@ -1,1 +1,9 @@
+<?php
 
+$conn=mysqli_connect("db4free.net","immust","immustimmust","immust");
+$result=mysqli_query($conn, "select * from user");
+#mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
+while ($row=mysqli_fetch_array($result)) {
+  echo $row["id"]." ".$row["pwd"]."<br>";
+} 
+?>
