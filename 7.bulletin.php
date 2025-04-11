818fc3f0249b8e50efb55a1bf133c748c76536eb
Lowei1225 <jack20041225@gmail.com>
2025-04-11 19:56:56 +0800

    Update 7.bulletin.php

--- 7.bulletin.php
+++ 7.bulletin.php
@@ -1,17 +1,20 @@
 <?php
-
-$conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-$result=mysqli_query($conn, "select * from bulletin");
-echo "<table border=1><tr><td>布告欄編號</td><td>布告型態</td><td>標題</td><td>布告內容</td>"
-while($row=mysqli_fetch_array($result)){
-    echo $row["bid"];
-    echo" ";
-    echo $row["type"];
-    echo" ";
-    echo $row["title"];
-    echo" ";
-    echo $row["content"];
-    echo" ";
-    echo $row["time"];
-    echo"<br>";
-}
+    error_reporting(0);
+    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+    $result=mysqli_query($conn, "select * from bulletin");
+    echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
+    while ($row=mysqli_fetch_array($result)){
+        echo "<tr><td>";
+        echo $row["bid"];
+        echo "</td><td>";
+        echo $row["type"];
+        echo "</td><td>"; 
+        echo $row["title"];
+        echo "</td><td>";
+        echo $row["content"]; 
+        echo "</td><td>";
+        echo $row["time"];
+        echo "</td></tr>";
+    }
+    echo "</table>"
+?>
